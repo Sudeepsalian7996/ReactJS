f00df4fe819a2f032c8b1d6866f09c2a17383905
Sudeepsalian7996 <sudeep7996>
2023-04-24 10:46:07 +0530

    js and css added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem(){
+    const date=new Date(2001,12,26)
+    const expense="LIC policy"
+    const money=200
     return (
-        <div>
-        <h2>Expense Items</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+        <div >
+        <h2 className="expense">Expense Items</h2>
+        <p>{date.toISOString()}</p>
+        <p>{expense}</p>
+        <p>{money}</p>
         </div>
     )
 }
