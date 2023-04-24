ca6406aa57d08bb98df9f5c5a4e93f5c8af8fb2b
Sudeepsalian7996 <sudeep7996>
2023-04-24 11:58:09 +0530

    Added a multiple expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      loc:"manglore"
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12),loc:"udupi" },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      loc:"banglore"
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      loc:"chennai"
+    },
+  ];
+
   return (
     <div>
       <h1>Lets go</h1>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((ele) => {
+        return <ExpenseItem title={ele.title} amount={ele.amount} date={ele.date} loc={ele.loc}></ExpenseItem>
+      })
+      }
+
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem(){
-    const date=new Date(2001,12,26)
-    const expense="LIC policy"
-    const money=200
+function ExpenseItem(props){
     return (
         <div >
         <h2 className="expense">Expense Items</h2>
-        <p>{date.toISOString()}</p>
-        <p>{expense}</p>
-        <p>{money}</p>
+        <p>{props.date.toISOString()}</p>
+        <p>{props.title}</p>
+        <p>{props.amount}</p>
+        <p>{props.loc}</p>
         </div>
     )
 }
